39f114fe43c8c8d6e575e302842e47c2addc528a
caitlinly <43999437+caitlinly@users.noreply.github.com>
2019-05-27 22:25:28 -0700

    workl

--- Unique Words/src/Main.java
+++ Unique Words/src/Main.java
@@ -5,7 +5,9 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
+        //Reads the file and puts lines into String then the lines into an Array list
         ArrayList<String[]> lines = new ArrayList<>();
+        //lines = [1 sentence [word1, word2], 2 sentence [word1, word2]]
         FileReader fr = new FileReader("illiad.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
@@ -14,20 +16,22 @@ public class Main{
         }
         br.close();
 
-        lines.subList(i,i+1);
-
+        //Adding the individual words into a Linked Hash Set
+        ArrayList<String> stringArrayList = new ArrayList<>();
         LinkedHashSet<Word> uniqueWord = new LinkedHashSet<>();
-        for(int i = 0; i < lines.size(); i++){
-            Word w = new Word();
-            uniqueWord.add(w);
+        // get the String[] from the array list, get the string from String[]
+        // make it an object of the word class
+        for(int i = 0; i < lines.size(); i++) {
+            for(int y = 0; y <  lines.get(i).length; y++){
+                uniqueWord.add(new Word( lines.get(i)[y]));
+            }
         }
+
         Iterator<Word> wordIterator = uniqueWord.iterator();
         while(wordIterator.hasNext()){
             System.out.println(wordIterator.next());
         }
 
-
     }
 
 }
-
